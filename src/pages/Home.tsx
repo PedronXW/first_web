a88a93b9e8065c95910b71a2b43f2c5e46bdbca2
Pedro Almeida <pedro.almeida@teleguardian.com.br>
2023-06-02 09:04:16 -0300

    Gráfico e lista do dashboard pt2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,13 +17,13 @@ const Home = () => {
             <section title="Dashboard" className="h-screen w-full grow-1 flex flex-col">
                 <Header />
                 <HeaderMobile />
-                <div className="h-full w-full grow-1 flex flex-col md:p-12 pb-3 pl-7 pr-6 md:pt-0 overflow-y-scroll lg:overflow-hidden gap-8">
+                <div className="h-full w-full grow-1 flex flex-col md:p-12 pb-3 pl-7 pr-6 md:pt-0 overflow-y-scroll lg:overflow-hidden gap-7">
                     <div className="grid lg:grid-cols-3 grid-cols-1 md:gap-2 xl:gap-8 w-full h-min">
                         <DashboardOutDoor name="Chamadas Recebidas" quantity_actually={20} quantity_past={10} icon={<PhoneCall className="text-secundary_color" size={20} />} />
                         <DashboardOutDoor name="Atendidas" quantity_actually={20} quantity_past={40} icon={<CheckFat className="text-secundary_color" size={20} />} />
                         <DashboardOutDoor name="Perdidas" quantity_actually={20} quantity_past={10} icon={<PhoneX className="text-secundary_color" size={20} />} />
                     </div>
-                    <div className="w-full grow-1 h-full flex-col lg:flex-row lg:flex gap-6">
+                    <div className="w-full grow-1 h-full flex-col lg:flex-row flex gap-7">
                         <DashboardChart/>
                         <DashboardCallList/>
                     </div>
