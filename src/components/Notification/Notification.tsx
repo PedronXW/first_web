0a7dabb473702a345eb09fb91e6f571a2586e102
PedronXW <pedroalmeidan@gmail.com>
2023-06-07 17:51:59 -0300

    notificação pt3

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,3 +1,4 @@
+import { House } from "@phosphor-icons/react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { removeNotification } from "../../slices/stateNotificationSlice";
 
@@ -8,14 +9,20 @@ const Notification=()=>{
     const actions = useAppDispatch();
 
     if(state==true){
-        setInterval(()=>{
+        setTimeout(()=>{
             actions(removeNotification())
         }, 4000)
     }
     
     return(
-        <div className={`h-20 w-72 bg-red-500 absolute bottom-4 right-4 ${state?"flex":"hidden"}`}>
-            
+        <div className={`h-16 w-72 bg-secundary_color drop-shadow-3xl rounded-md border-[1px] p-1 px-5 border-primary_color items-center absolute bottom-4 right-4 ${state?"flex":"hidden"}`}>
+            <figure className="w-10">
+                <House size={20}/>
+            </figure>
+            <div className="flex flex-col justify-start">
+                <h2>Nome da atividade</h2>
+                <h3>ID da atividade</h3>
+            </div>
         </div>
     )
 }
