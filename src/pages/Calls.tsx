7aa3cc630e755f0ddd4ad2e13353f4e1aed15430
PedronXW <pedroalmeidan@gmail.com>
2023-06-27 17:57:13 -0300

    Início do desenvolvimento da interface para adicionar novos ramais

--- src/pages/Calls.tsx
+++ src/pages/Calls.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ArrowULeftDown, PhoneIncoming, PhoneOutgoing } from "@phosphor-icons/react";
+import { ArrowULeftDown, MagnifyingGlass, PhoneIncoming, PhoneOutgoing } from "@phosphor-icons/react";
 import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -54,6 +54,7 @@ const Calls = () => {
     const [busyCallStatus, setBusyCallStatus] = useState<boolean>(true);
     const [timeoutedCallStatus, setTimeoutedCallStatus] = useState<boolean>(true);
     const [canceledCallStatus, setCanceledCallStatus] = useState<boolean>(true);
+    const [searchLayoutStatus, setSearchLayoutStatus] = useState<boolean>(false);
 
     return (
         <div className="h-screen w-screen flex flex-col md:flex-row bg-background_color overflow-hidden">
@@ -66,48 +67,54 @@ const Calls = () => {
                 </div>
                 <BottomNavigationMenu selected={2} />
             </div>
-            <div className="h-full min-w-[320px] max-w-[320px] px-5 pt-5 hidden lg:flex lg:flex-col bg-secundary_color drop-shadow-lg gap-5 overflow-y-scroll pb-4">
-                <DatePicker onEndSelected={setEndSelected} onStartSelected={setStartSelected} />
-                <SearchInput register={register} focus={setFocus} />
-                <h2 className="text-primary_color font-bold text-lg">Tipo de chamada</h2>
-                <div className="flex w-full h-min">
-                    <button type="button" onClick={() => { setIncommingCallsStatus(!incommingCallsStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${incommingCallsStatus ? "border-green-700" : "border-primary_color"} rounded-l-lg drop-shadow-3xl`}>
-                        <PhoneIncoming size={20} className={`-ml-[5px] ${incommingCallsStatus ? "text-green-700" : "text-primary_color"}`} />
-                        <p className={`${incommingCallsStatus ? "text-green-700" : "text-primary_color"}`}>Recebida</p>
-                    </button>
-                    <button type="button" onClick={() => { setLocalCallsStatus(!localCallsStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${localCallsStatus ? "border-yellow-700" : "border-primary_color"} drop-shadow-3xl`}>
-                        <ArrowULeftDown size={20} className={`-ml-[5px] ${localCallsStatus ? "text-yellow-700" : "text-primary_color"}`} />
-                        <p className={`${localCallsStatus ? "text-yellow-700" : "text-primary_color"}`}>Interna</p>
-                    </button>
-                    <button type="button" onClick={() => { setOutgoingCallsStatus(!outgoingCallsStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${outgoingCallsStatus ? "border-blue-700" : "border-primary_color"} rounded-r-lg drop-shadow-3xl`}>
-                        <PhoneOutgoing size={20} className={`-ml-[5px] ${outgoingCallsStatus ? "text-blue-700" : "text-primary_color"}`} />
-                        <p className={`${outgoingCallsStatus ? "text-blue-700" : "text-primary_color"}`}>Realizada</p>
-                    </button>
-                </div>
-                <h2 className="text-primary_color font-bold text-lg">Resultado de chamada</h2>
-                <div className="flex flex-col w-full">
+            <div className={` h-full w-screen z-40 lg:w-min absolute lg:relative ${searchLayoutStatus?"flex":"hidden lg:flex"}`}>
+                <div className="h-full flex grow-1 w-full bg-black opacity-30" />
+                <div className="h-full min-w-[320px] max-w-[320px] px-5 pt-5 flex flex-col bg-secundary_color drop-shadow-lg gap-5 overflow-y-scroll pb-4">
+                    <DatePicker onEndSelected={setEndSelected} onStartSelected={setStartSelected} />
+                    <SearchInput register={register} focus={setFocus} />
+                    <h2 className="text-primary_color font-bold text-lg">Tipo de chamada</h2>
                     <div className="flex w-full h-min">
-                        <button type="button" onClick={() => { setAnsweredCallStatus(!answeredCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${answeredCallStatus ? "border-green-700" : "border-primary_color"} rounded-tl-lg drop-shadow-3xl`}>
-                            <PhoneIncoming size={20} className={`-ml-[5px] ${answeredCallStatus ? "text-green-700" : "text-primary_color"}`} />
-                            <p className={`${answeredCallStatus ? "text-green-700" : "text-primary_color"}`}>Atendida</p>
+                        <button type="button" onClick={() => { setIncommingCallsStatus(!incommingCallsStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${incommingCallsStatus ? "border-green-700" : "border-primary_color"} rounded-l-lg drop-shadow-3xl`}>
+                            <PhoneIncoming size={20} className={`-ml-[5px] ${incommingCallsStatus ? "text-green-700" : "text-primary_color"}`} />
+                            <p className={`${incommingCallsStatus ? "text-green-700" : "text-primary_color"}`}>Recebida</p>
                         </button>
-                        <button type="button" onClick={() => { setTimeoutedCallStatus(!timeoutedCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${timeoutedCallStatus ? "border-yellow-700" : "border-primary_color"} rounded-tr-lg drop-shadow-3xl`}>
-                            <ArrowULeftDown size={20} className={`-ml-[5px] ${timeoutedCallStatus ? "text-yellow-700" : "text-primary_color"}`} />
-                            <p className={`${timeoutedCallStatus ? "text-yellow-700" : "text-primary_color"}`}>Perdida</p>
+                        <button type="button" onClick={() => { setLocalCallsStatus(!localCallsStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${localCallsStatus ? "border-yellow-700" : "border-primary_color"} drop-shadow-3xl`}>
+                            <ArrowULeftDown size={20} className={`-ml-[5px] ${localCallsStatus ? "text-yellow-700" : "text-primary_color"}`} />
+                            <p className={`${localCallsStatus ? "text-yellow-700" : "text-primary_color"}`}>Interna</p>
+                        </button>
+                        <button type="button" onClick={() => { setOutgoingCallsStatus(!outgoingCallsStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${outgoingCallsStatus ? "border-blue-700" : "border-primary_color"} rounded-r-lg drop-shadow-3xl`}>
+                            <PhoneOutgoing size={20} className={`-ml-[5px] ${outgoingCallsStatus ? "text-blue-700" : "text-primary_color"}`} />
+                            <p className={`${outgoingCallsStatus ? "text-blue-700" : "text-primary_color"}`}>Realizada</p>
                         </button>
                     </div>
-                    <div className="flex w-full h-min">
-                        <button type="button" onClick={() => { setBusyCallStatus(!busyCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${busyCallStatus ? "border-blue-700" : "border-primary_color"} rounded-bl-lg drop-shadow-3xl`}>
-                            <PhoneIncoming size={20} className={`-ml-[5px] ${busyCallStatus ? "text-blue-700" : "text-primary_color"}`} />
-                            <p className={`${busyCallStatus ? "text-blue-700" : "text-primary_color"}`}>Ocupada</p>
-                        </button>
-                        <button type="button" onClick={() => { setCanceledCallStatus(!canceledCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${canceledCallStatus ? "border-red-700" : "border-primary_color"} rounded-br-lg drop-shadow-3xl`}>
-                            <ArrowULeftDown size={20} className={`-ml-[5px] ${canceledCallStatus ? "text-red-700" : "text-primary_color"}`} />
-                            <p className={`${canceledCallStatus ? "text-red-700" : "text-primary_color"}`}>Cancelada</p>
-                        </button>
+                    <h2 className="text-primary_color font-bold text-lg">Resultado de chamada</h2>
+                    <div className="flex flex-col w-full">
+                        <div className="flex w-full h-min">
+                            <button type="button" onClick={() => { setAnsweredCallStatus(!answeredCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${answeredCallStatus ? "border-green-700" : "border-primary_color"} rounded-tl-lg drop-shadow-3xl`}>
+                                <PhoneIncoming size={20} className={`-ml-[5px] ${answeredCallStatus ? "text-green-700" : "text-primary_color"}`} />
+                                <p className={`${answeredCallStatus ? "text-green-700" : "text-primary_color"}`}>Atendida</p>
+                            </button>
+                            <button type="button" onClick={() => { setTimeoutedCallStatus(!timeoutedCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${timeoutedCallStatus ? "border-yellow-700" : "border-primary_color"} rounded-tr-lg drop-shadow-3xl`}>
+                                <ArrowULeftDown size={20} className={`-ml-[5px] ${timeoutedCallStatus ? "text-yellow-700" : "text-primary_color"}`} />
+                                <p className={`${timeoutedCallStatus ? "text-yellow-700" : "text-primary_color"}`}>Perdida</p>
+                            </button>
+                        </div>
+                        <div className="flex w-full h-min">
+                            <button type="button" onClick={() => { setBusyCallStatus(!busyCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${busyCallStatus ? "border-blue-700" : "border-primary_color"} rounded-bl-lg drop-shadow-3xl`}>
+                                <PhoneIncoming size={20} className={`-ml-[5px] ${busyCallStatus ? "text-blue-700" : "text-primary_color"}`} />
+                                <p className={`${busyCallStatus ? "text-blue-700" : "text-primary_color"}`}>Ocupada</p>
+                            </button>
+                            <button type="button" onClick={() => { setCanceledCallStatus(!canceledCallStatus) }} className={`p-2 bg-white flex flex-col flex-1 justify-center items-center gap-2 border-2 ${canceledCallStatus ? "border-red-700" : "border-primary_color"} rounded-br-lg drop-shadow-3xl`}>
+                                <ArrowULeftDown size={20} className={`-ml-[5px] ${canceledCallStatus ? "text-red-700" : "text-primary_color"}`} />
+                                <p className={`${canceledCallStatus ? "text-red-700" : "text-primary_color"}`}>Cancelada</p>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <button className={`absolute lg:hidden p-4 drop-shadow-3xl rounded-full bottom-20 md:bottom-3 bg-white z-50 ${searchLayoutStatus?"right-[330px]":"right-2"}  `}>
+                <MagnifyingGlass size={20} className="text-primary_color" onClick={() => { setSearchLayoutStatus(!searchLayoutStatus) }} />
+            </button>
         </div>
     )
 }
--- src/pages/Ramais.tsx
+++ src/pages/Ramais.tsx
@@ -1,25 +1,52 @@
+import { Bell, Plus } from "@phosphor-icons/react";
+import { useEffect, useRef, useState } from "react";
 import BottomNavigationMenu from "../components/BottomNavigationMenu/BottomNavigationMenu";
 import Header from "../components/Header/Header";
 import HeaderMobile from "../components/Header/HeaderMobile";
 import Drawer from "../components/Lists/Drawer/Drawer";
 import RamaisList from "../components/Lists/RamaisList/RamaisList";
-import AddRamalButton from "../components/SuspendedComponents/AddRamalButton/AddRamalButton";
 
 const Ramais = () => {
+
+    const [state, setState] = useState(false);
+    const ref = useRef<any>();
+    const refe = useRef<any>();
+    useEffect(() => {
+        document.addEventListener('mousedown', (event) => {
+            console.log(event.target);
+            console.log(ref.current.children)
+            for(let i = 0; i < ref.current.children.length; i++){
+                console.log(ref.current.children[i].outerHTML);
+                if(state === true && ref.current){
+                    setState(false);
+                }
+            }
+        });
+    });
 
     return (
         <div className="h-screen w-screen flex flex-col md:flex-row bg-background_color">
             <Drawer selected={1} />
             <div className="w-full flex flex-col grow-1 overflow-hidden md:shadow-inner">
                 <HeaderMobile />
-                <div className="grow-1 w-full flex flex-col overflow-y-scroll">
+                <div ref={refe} className="grow-1 w-full flex flex-col overflow-y-scroll">
                     <Header title="Ramais">
-                        <AddRamalButton />
+                        <ul className='flex flex-col gap-2 items-end' ref={ref}>
+                            <div className="justify-center items-center p-2 cursor-pointer" onClick={(event: any) => { setState(!state); event.stopPropagation() }}>
+                                <Bell size={24} />
+                            </div>
+                            <li className={`${state ? "flex" : "hidden"} relative bg-white border-2 border-background_color z-50 rounded-md w-72 min-h-[300px] drop-shadow-3xl`} onClick={(event: any) => { event.stopPropagation() }}>
+                                <div>Arroz</div>
+                            </li>
+                        </ul>
                     </Header>
                     <RamaisList />
                 </div>
                 <BottomNavigationMenu selected={1} />
             </div>
+            <button className={`absolute lg:hidden p-4 drop-shadow-3xl rounded-full bottom-20 md:bottom-3 bg-white z-50 right-2 `}>
+                <Plus size={20} className="text-primary_color" />
+            </button>
         </div>
     )
 }
